930b38956e19dbde5a5571b1d22c15af97e8ddfe
Duffel_Kerfuffle <maulik.tripathi@gmail.com>
2021-09-01 21:35:32 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	documents "chatoverchai/docs"
+	"chatoverchai/storage"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	poetryManager := documents.DocumentManager{}
+	blogManager := documents.DocumentManager{}
+	previousManager := documents.DocumentManager{}
+	upcomingManager := documents.DocumentManager{}
+	photoManager := documents.DocumentManager{}
+	videoManager := documents.DocumentManager{}
+
+	storage.Load(&poetryManager, "poetry")
+	storage.Load(&blogManager, "blog")
+	storage.Load(&previousManager, "previous")
+	storage.Load(&upcomingManager, "upcoming")
+	storage.Load(&photoManager, "photo")
+	storage.Load(&videoManager, "videos")
+
+	fmt.Println(poetryManager)
+	fmt.Println(blogManager)
+	fmt.Println(previousManager)
+	fmt.Println(upcomingManager)
+	fmt.Println(photoManager)
+	fmt.Println(videoManager)
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+	r.Delims("[[", "]]")
+	r.LoadHTMLGlob("*.html")
+
+	r.Use(static.Serve("/js", static.LocalFile("./js", true)))
+	r.Use(static.Serve("/images", static.LocalFile("./images", true)))
+	r.Use(static.Serve("/videos", static.LocalFile("./videos", true)))
+	r.Use(static.Serve("/TestVideo", static.LocalFile("./TestVideo", true)))
+
+	r.GET("/poetry", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "poetry.html", poetryManager)
+	})
+
+	r.GET("/blog", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "Blog2.html", blogManager)
+	})
+
+	r.GET("/upcoming", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "upcoming.html", previousManager)
+	})
+
+	r.GET("/previous", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "previous.html", upcomingManager)
+	})
+
+	r.GET("/photos", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "Photos.html", photoManager)
+	})
+
+	r.GET("/vids", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "vids.html", videoManager)
+	})
+
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "homepage.html", nil)
+	})
+
+	r.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about.html", nil)
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/home")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000" // Default port if not specified
+	}
+	err := r.Run(":" + port)
+	if err != nil {
+		panic(err)
+	}
+}
